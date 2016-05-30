2836da66133a8cc01e1d87be2a4f76127dcbd43d
Alexandr Tumaykin <alexandrtumaykin@gmail.com>
2016-05-30 10:28:21 +0300

    change composer.json add function getMailFromRaw

--- src/PhpImap/IncomingMail.php
+++ src/PhpImap/IncomingMail.php
@@ -1,66 +1,76 @@
 <?php namespace PhpImap;
 
 /**
- * @see https://github.com/barbushin/php-imap
+ * @see    https://github.com/barbushin/php-imap
  * @author Barbushin Sergey http://linkedin.com/in/barbushin
  */
-class IncomingMail {
+class IncomingMail
+{
 
-	public $id;
-	public $date;
-	public $subject;
+    public $id;
+    public $date;
+    public $subject;
 
-	public $fromName;
-	public $fromAddress;
+    public $fromName;
+    public $fromAddress;
 
-	public $to = array();
-	public $toString;
-	public $cc = array();
-	public $replyTo = array();
+    public $to = [];
+    public $toString;
+    public $cc = [];
+    public $replyTo = [];
 
-	public $messageId;
+    public $messageId;
 
-	public $textPlain;
-	public $textHtml;
-	/** @var IncomingMailAttachment[] */
-	protected $attachments = array();
+    public $textPlain;
+    public $textHtml;
+    /** @var IncomingMailAttachment[] */
+    protected $attachments = [];
 
-	public function addAttachment(IncomingMailAttachment $attachment) {
-		$this->attachments[$attachment->id] = $attachment;
-	}
+    public function addAttachment(IncomingMailAttachment $attachment)
+    {
+        $this->attachments[$attachment->id] = $attachment;
+    }
 
-	/**
-	 * @return IncomingMailAttachment[]
-	 */
-	public function getAttachments() {
-		return $this->attachments;
-	}
+    /**
+     * @return IncomingMailAttachment[]
+     */
+    public function getAttachments()
+    {
+        return $this->attachments;
+    }
 
-	/**
-	 * Get array of internal HTML links placeholders
-	 * @return array attachmentId => link placeholder
-	 */
-	public function getInternalLinksPlaceholders() {
-		return preg_match_all('/=["\'](ci?d:([\w\.%*@-]+))["\']/i', $this->textHtml, $matches) ? array_combine($matches[2], $matches[1]) : array();
+    /**
+     * Get array of internal HTML links placeholders
+     *
+     * @return array attachmentId => link placeholder
+     */
+    public function getInternalLinksPlaceholders()
+    {
+        return preg_match_all('/=["\'](ci?d:([\w\.%*@-]+))["\']/i', $this->textHtml,
+            $matches) ? array_combine($matches[2], $matches[1]) : [];
 
-	}
+    }
 
-	public function replaceInternalLinks($baseUri) {
-		$baseUri = rtrim($baseUri, '\\/') . '/';
-		$fetchedHtml = $this->textHtml;
-		foreach($this->getInternalLinksPlaceholders() as $attachmentId => $placeholder) {
-			if(isset($this->attachments[$attachmentId])) {
-				$fetchedHtml = str_replace($placeholder, $baseUri . basename($this->attachments[$attachmentId]->filePath), $fetchedHtml);
-			}
-		}
-		return $fetchedHtml;
-	}
+    public function replaceInternalLinks($baseUri)
+    {
+        $baseUri = rtrim($baseUri, '\\/') . '/';
+        $fetchedHtml = $this->textHtml;
+        foreach ($this->getInternalLinksPlaceholders() as $attachmentId => $placeholder) {
+            if (isset($this->attachments[$attachmentId])) {
+                $fetchedHtml = str_replace($placeholder,
+                    $baseUri . basename($this->attachments[$attachmentId]->filePath), $fetchedHtml);
+            }
+        }
+
+        return $fetchedHtml;
+    }
 }
 
-class IncomingMailAttachment {
+class IncomingMailAttachment
+{
 
-	public $id;
-	public $name;
-	public $filePath;
-	public $disposition;
+    public $id;
+    public $name;
+    public $filePath;
+    public $disposition;
 }
